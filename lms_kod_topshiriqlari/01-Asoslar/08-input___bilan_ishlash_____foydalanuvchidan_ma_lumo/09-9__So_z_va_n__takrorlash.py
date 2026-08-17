a010c67389103b91db7c6a5c2ff77c271008b365
furqatxasanboyev48-cmyk <furqatxasanboyev48@gmail.com>
2026-08-17 14:58:50 +0500

    Add: 9) So‘z va n: takrorlash [furqatxasanboyev48-cmyk]

---
+++ lms_kod_topshiriqlari/01-Asoslar/08-input___bilan_ishlash_____foydalanuvchidan_ma_lumo/09-9__So_z_va_n__takrorlash.py
@@ -0,0 +1,7 @@
+# 1-qator: so'z
+# 2-qator: n (butun son)
+# So'zni n marta bo'shliq bilan ajratib chiqarib bering.
+# Masalan: word=hi, n=3 -> "hi hi hi"
+soz = input()
+n = int(input())
+print(((soz + " ") * n).strip())
