d1f915b00b98ea8590529da239adc11829d08940
furqatxasanboyev48-cmyk <furqatxasanboyev48@gmail.com>
2026-08-17 14:59:19 +0500

    Add: 11) "ism,age" ko‘rinishidagi ma’lumotni ajratish [furqatxasanboyev48-cmyk]

---
+++ lms_kod_topshiriqlari/01-Asoslar/08-input___bilan_ishlash_____foydalanuvchidan_ma_lumo/11-11___ism_age__ko_rinishidagi_ma_lumotni_ajratish.py
@@ -0,0 +1,7 @@
+# Masalan: "Aziz,25" ko'rinishida satr kiriting.
+# Vergul bo'yicha ajrating.
+# 1-qator: "Name: <ism>"
+# 2-qator: "Age: <age>"
+name, age = input().split(',')
+print(f"Name: {name}")
+print(f"Age: {age}")
