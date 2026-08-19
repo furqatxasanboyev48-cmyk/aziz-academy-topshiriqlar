c6956ab939ee87e1023192f5181e0d07bef275d2
furqatxasanboyev48-cmyk <furqatxasanboyev48@gmail.com>
2026-08-19 14:41:37 +0500

    Add: 9) Sekund → daqiqa va qoldiq (// va %) [furqatxasanboyev48-cmyk]

---
+++ lms_kod_topshiriqlari/01-Asoslar/09-Arifmetik_operatorlar_____-_____________va_priorit/09-9__Sekund___daqiqa_va_qoldiq_____va___.py
@@ -0,0 +1,8 @@
+# seconds (butun son)
+# minutes = seconds // 60
+# rem = seconds % 60
+# 1-qator: "Min: <minutes>"
+# 2-qator: "Sec: <rem>"
+seconds = int(input())
+print("Min:", seconds // 60)
+print("Sec:", seconds % 60)
