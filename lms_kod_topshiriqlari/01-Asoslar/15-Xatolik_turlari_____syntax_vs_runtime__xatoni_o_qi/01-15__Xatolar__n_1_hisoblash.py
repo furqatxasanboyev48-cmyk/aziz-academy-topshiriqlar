4bb684e2b73923806d0495b23cbfe22d18b38b78
furqatxasanboyev48-cmyk <furqatxasanboyev48@gmail.com>
2026-09-04 16:30:07 +0500

    Add: 15) Xatolar: n+1 hisoblash [furqatxasanboyev48-cmyk]

---
+++ lms_kod_topshiriqlari/01-Asoslar/15-Xatolik_turlari_____syntax_vs_runtime__xatoni_o_qi/01-15__Xatolar__n_1_hisoblash.py
@@ -0,0 +1,17 @@
+# Bitta butun son n kiriting.
+# Konsolga n+1 qiymatini chiqaring.
+# (Bu topshiriqda syntax va runtime xatolarni oldini olish muhim.)
+n = int(input())
+print(n + 1)
+
+
+
+
+
+
+
+
+
+
+
+
