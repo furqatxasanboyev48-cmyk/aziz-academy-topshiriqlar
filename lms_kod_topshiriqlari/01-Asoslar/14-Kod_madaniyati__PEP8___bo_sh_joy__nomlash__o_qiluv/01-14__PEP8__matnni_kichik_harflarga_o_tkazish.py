3a9d96852e8077e9af7fbeb2c211386eb32191cd
furqatxasanboyev48-cmyk <furqatxasanboyev48@gmail.com>
2026-09-04 15:17:31 +0500

    Add: 14) PEP8: matnni kichik harflarga o'tkazish [furqatxasanboyev48-cmyk]

---
+++ lms_kod_topshiriqlari/01-Asoslar/14-Kod_madaniyati__PEP8___bo_sh_joy__nomlash__o_qiluv/01-14__PEP8__matnni_kichik_harflarga_o_tkazish.py
@@ -0,0 +1,6 @@
+# Bitta satr matn kiriting.
+# Uni kichik harflarga o'tkazib chiqaring.
+# Izoh: o'zgaruvchi nomlari PEP8 bo'yicha snake_case bo'lsin (masalan: user_text).
+user_text = input()
+print(user_text.lower())
+
